e7617a257cfa8d55e194264830668680c65768c0
gooooya <138767327+gooooya@users.noreply.github.com>
2023-11-16 15:39:38 +0900

    tmpフォルダを作成するよう修正

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,19 @@
 REM @echo off
 
 REM Skip build if both zip files exist
+IF NOT EXIST "tmp\" (
+    MKDIR "tmp"
+    GOTO StartBuild
+)
+
 IF EXIST "tmp\scrapy.zip" (
     IF EXIST "tmp\layer.zip" (
         ECHO Skip build
         GOTO EndBuild
     )
 )
+
+:StartBuild
 
 REM Build Docker image if either of the zip files does not exist
 cd lambda_layer
